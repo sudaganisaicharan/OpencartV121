0655d70589f994e35b66d33ce4210e305716413c
Saicharan <sudaganisaicharan@gmail.com>
2024-08-27 00:55:30 +0530

    first commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Selenium webdriver\OpencartV121\
+mvn test
---
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -0,0 +1,63 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.AccountRegistrationPage;
+import pageObjects.HomePage;
+import testBase.BaseClass;
+
+public class TC001_AccountRegistrationTest extends BaseClass{
+	
+	
+	@Test(groups={"Regression", "Master"})
+	public void verify_account_registration()
+	{
+		
+		logger.info("******* Starting TC001_AccountRegistrationTest *****");
+			
+		try {
+			HomePage hp = new HomePage(driver);
+			hp.clickMyAccount();
+			logger.info("Clicked on MyAccount Link...");
+			
+			hp.clickRegister();
+			logger.info("Clicked on Register Link...");
+			
+			AccountRegistrationPage regpage = new AccountRegistrationPage(driver);
+			
+			logger.info("Providing customer details...");
+			regpage.setFirstName(randomString().toUpperCase());
+			regpage.setLastName(randomString().toUpperCase());
+			regpage.setEmail(randomString()+"@gmail.com");// randomly generated the email
+			regpage.setTelephone(randomNumber());
+			
+			String password=randomAlphaNumeric();
+			
+			regpage.setPassword(password);
+			regpage.setConfirmPassword(password);
+			
+			regpage.setPrivacyPolicy();
+			regpage.clickContinue();
+			
+			logger.info("Validating expected message...");
+			String confmsg=regpage.getConfirmationMsg();
+			if(confmsg.equals("Your Account Has Been Created!")) {
+				Assert.assertTrue(true);
+			}
+			else {
+				logger.error("Test failed...");
+				logger.debug("Debug logs");
+				Assert.assertTrue(false);
+			}
+			//Assert.assertEquals(confmsg, "Your Account Has Been Created!");
+		}
+		catch(Exception e) {
+			
+			Assert.fail();
+		}
+	}
+	
+	
+	
+}
